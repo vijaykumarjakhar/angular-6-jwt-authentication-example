8d2bc1bd36e91b5757900022fc4b86c8001af301
vijaykumarjakhar <vijayjakhar2010@gmail.com>
2021-02-22 20:41:42 +0530

    angular-6-jwt-authentication-example

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,16 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../_models';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${config.apiUrl}/users`);
+    }
+    getAllNext() {
+        return this.http.get<User[]>('http://localhost:51215/users');
+    }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { UserService } from '../_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+    users: User[] = [];
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit() {
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.users = users;
+        });
+    }
+    clickme() {
+
+        this.userService.getAllNext().pipe(first()).subscribe(users => {
+            this.users = users;
+            console.log(JSON.stringify(users));
+        });
+    }
+}
